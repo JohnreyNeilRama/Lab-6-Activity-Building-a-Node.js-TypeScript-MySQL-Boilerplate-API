e334b3d0be4926ab176b3e1f2b098f5e6b83f44e
unknown <YY640@ccs.local>
2026-05-22 16:16:26 +0800

    Fix swagger

--- _helpers/swagger.ts
+++ _helpers/swagger.ts
@@ -4,7 +4,7 @@ const router = express.Router();
 import swaggerUi from 'swagger-ui-express';
 import YAML from 'yamljs';
 
-const swaggerDocument = YAML.load(path.join(__dirname, '../swagger.yaml'));
+const swaggerDocument = YAML.load(path.join(__dirname, './swagger.yaml'));
 
 router.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
